510d4a32e3d6c53f8d64fa1622b983937794eb12
Satoshi Konno <skonno@cybergarage.org>
2014-10-07 12:07:17 +0900

    * Added HTTPSimpleServerList to check HTTPServerList class.

--- tests/HTTPServerTests.cpp
+++ tests/HTTPServerTests.cpp
@@ -21,7 +21,7 @@ using namespace std;
 using namespace uHTTP;
 
 //////////////////////////////////////////////////////////////////////
-// Simple Server
+// Simple Request Listener
 //////////////////////////////////////////////////////////////////////
 
 #define UHTTP_HTTP_SERVER_TEST_LOOP_COUNT 100
@@ -32,6 +32,64 @@ class HTTPSimpleRequestListener : public HTTPRequestListener
 {
   uHTTP::HTTP::StatusCode httpRequestRecieved(HTTPRequest *httpReq);
 };
+
+uHTTP::HTTP::StatusCode HTTPSimpleRequestListener::httpRequestRecieved(HTTPRequest *httpReq)
+{
+  if (httpReq->isGetRequest() == false) {
+    return httpReq->returnBadRequest();
+  }
+  
+  std::string uri;
+  httpReq->getURI(uri);
+  if (uri.length() <= 0) {
+    return httpReq->returnBadRequest();
+  }
+  
+  string paramStr;
+  BOOST_CHECK(httpReq->getParameterValue(UHTTP_HTTP_SERVER_TEST_PARAM_NAME, &paramStr));
+  
+  std::string msg = UHTTP_HTTP_SERVER_TEST_CONTENT;
+  msg.append(paramStr);
+  
+  HTTPResponse httpRes;
+  httpRes.setStatusCode(HTTP::OK_REQUEST);
+  httpRes.setContent(msg);
+  httpReq->post(&httpRes);
+  
+  return uHTTP::HTTP::OK_REQUEST;
+}
+
+//////////////////////////////////////////////////////////////////////
+// Simple Server
+//////////////////////////////////////////////////////////////////////
+
+static void RequestToHTTPServer(const std::string &ifaddr, int port)
+{
+  for (int n=0; n<UHTTP_HTTP_SERVER_TEST_LOOP_COUNT; n++) {
+    HTTPRequest httpReq;
+    httpReq.setMethod(HTTP::GET);
+    char httpReqURL[64];
+    snprintf(httpReqURL, sizeof(httpReqURL), "http://%s:%d/?%s=%d", ifaddr.c_str(), port, UHTTP_HTTP_SERVER_TEST_PARAM_NAME, n);
+    httpReq.setURL(httpReqURL);
+    
+    string httpReqStr;
+    httpReq.toString(httpReqStr);
+    BOOST_MESSAGE(httpReqStr);
+    
+    HTTPResponse *httpRes = httpReq.post();
+    BOOST_CHECK_EQUAL(httpRes->getStatusCode(), 200);
+    string httpResContent = httpRes->getContent();
+    BOOST_CHECK(0 < httpResContent.length());
+    
+    std::stringstream msgBuf;
+    msgBuf << UHTTP_HTTP_SERVER_TEST_CONTENT << n;
+    BOOST_CHECK_EQUAL(httpResContent.compare(msgBuf.str()), 0);
+    
+    string httpResStr;
+    httpRes->toString(httpResStr);
+    BOOST_MESSAGE(httpResStr);
+  }
+}
 
 BOOST_AUTO_TEST_CASE(HTTPSimpleServer)
 {
@@ -47,30 +105,8 @@ BOOST_AUTO_TEST_CASE(HTTPSimpleServer)
   BOOST_CHECK(httpServer.open(httpPort));
   BOOST_CHECK(httpServer.start());
   
-  for (int n=0; n<UHTTP_HTTP_SERVER_TEST_LOOP_COUNT; n++) {
-    HTTPRequest httpReq;
-    httpReq.setMethod(HTTP::GET);
-    char httpReqURL[64];
-    snprintf(httpReqURL, sizeof(httpReqURL), "http://127.0.0.1:%d/?%s=%d", httpPort, UHTTP_HTTP_SERVER_TEST_PARAM_NAME, n);
-    httpReq.setURL(httpReqURL);
-    
-    string httpReqStr;
-    httpReq.toString(httpReqStr);
-    BOOST_MESSAGE(httpReqStr);
-    
-    HTTPResponse *httpRes = httpReq.post();
-    BOOST_CHECK_EQUAL(httpRes->getStatusCode(), 200);
-    string httpResContent = httpRes->getContent();
-    BOOST_CHECK(0 < httpResContent.length());
-
-    std::stringstream msgBuf;
-    msgBuf << UHTTP_HTTP_SERVER_TEST_CONTENT << n;
-    BOOST_CHECK_EQUAL(httpResContent.compare(msgBuf.str()), 0);
-    
-    string httpResStr;
-    httpRes->toString(httpResStr);
-    BOOST_MESSAGE(httpResStr);
-  }
+  const std::string ifaddr = "127.0.0.1";
+  RequestToHTTPServer(ifaddr, httpPort);
   
   BOOST_CHECK(httpServer.close());
   BOOST_CHECK(httpServer.stop());
@@ -86,28 +122,30 @@ BOOST_AUTO_TEST_CASE(HTTPSimpleServer)
   BOOST_CHECK_EQUAL(Socket::GetInstanceCount(), 0);
 }
 
-uHTTP::HTTP::StatusCode HTTPSimpleRequestListener::httpRequestRecieved(HTTPRequest *httpReq)
+BOOST_AUTO_TEST_CASE(HTTPSimpleServerList)
 {
-   if (httpReq->isGetRequest() == false) {
-      return httpReq->returnBadRequest();
-   }
-
-  std::string uri;
-  httpReq->getURI(uri);
-  if (uri.length() <= 0) {
-    return httpReq->returnBadRequest();
-  }
-
-  string paramStr;
-  BOOST_CHECK(httpReq->getParameterValue(UHTTP_HTTP_SERVER_TEST_PARAM_NAME, &paramStr));
-
-  std::string msg = UHTTP_HTTP_SERVER_TEST_CONTENT;
-  msg.append(paramStr);
+  BOOST_CHECK_EQUAL(Socket::GetInstanceCount(), 0);
   
-  HTTPResponse httpRes;
-  httpRes.setStatusCode(HTTP::OK_REQUEST);
-  httpRes.setContent(msg);
-  httpReq->post(&httpRes);
-
-  return uHTTP::HTTP::OK_REQUEST;
+  HTTPServerList httpServerList;
+  HTTPSimpleRequestListener httpSimpleReqListener;
+  httpServerList.addRequestListener(&httpSimpleReqListener);
+  
+  Random rand (1000, 10000);
+  int httpPort = rand.rand();
+  
+  BOOST_CHECK(httpServerList.open(httpPort));
+  BOOST_CHECK(httpServerList.start());
+  
+  size_t htttpServerCount = httpServerList.size();
+  BOOST_CHECK(0 < htttpServerCount);
+  for (size_t n= 0; n < htttpServerCount; n++) {
+    HTTPServer *httpServer = httpServerList.getHTTPServer(n);
+    const std::string ifaddr = httpServer->getAddress();
+    //RequestToHTTPServer(ifaddr, httpPort);
+  }
+  
+  BOOST_CHECK(httpServerList.close());
+  BOOST_CHECK(httpServerList.stop());
+  
+  BOOST_CHECK_EQUAL(Socket::GetInstanceCount(), 0);
 }
