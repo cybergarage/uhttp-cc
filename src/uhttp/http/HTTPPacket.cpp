6617038db37e9246e2c7983c4c5d4bdfbc8cde39
Satoshi Konno <skonno@cybergarage.org>
2014-12-11 05:01:48 +0900

    * HTTPPacket::getHeader(cost std::string&) to run more fast.

--- src/uhttp/http/HTTPPacket.cpp
+++ src/uhttp/http/HTTPPacket.cpp
@@ -92,16 +92,16 @@ const char *HTTPPacket::getFirstLineToken(int num, string &tokenBuf) {
 ////////////////////////////////////////////////
 
 HTTPHeader *HTTPPacket::getHeader(size_t n) {
-  size_t nHeaders = getNHeaders();
+  size_t nHeaders = httpHeaderList.size();
   if (nHeaders < (n+1))
     return NULL;
   return httpHeaderList.at(n);
 }
 
 HTTPHeader *HTTPPacket::getHeader(const std::string &name) {
-  size_t nHeaders = getNHeaders();
+  size_t nHeaders = httpHeaderList.size();
   for (size_t n = 0; n < nHeaders; n++) {
-    HTTPHeader *header = getHeader(n);
+    HTTPHeader *header = httpHeaderList.at(n);
     if (!header)
       continue;
     const char *headerName = header->getName();
