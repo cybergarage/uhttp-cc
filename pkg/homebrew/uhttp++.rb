9d7ae36c505561d3152f51542d6899c5c1b69b1c
Satoshi Konno <skonno@cybergarage.org>
2015-01-22 22:27:00 +0900

    * Updated uhttp++.rb for Homebrew.

--- pkg/homebrew/uhttp++.rb
+++ pkg/homebrew/uhttp++.rb
@@ -3,13 +3,16 @@ class Uhttpxx < Formula
   url "https://github.com/cybergarage/uHTTP4CC/archive/0.8.tar.gz"
   sha1 "b1ceb9f266d8465c1700be0a557efc7a5495786c"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "readline"
 
   def install
+    system "./bootstrap"
     system "./configure_macosx", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+                                 "--disable-dependency-tracking",
+                                 "--disable-silent-rules",
+                                 "--prefix=#{prefix}"
     system "make"
     system "make", "install"
   end
