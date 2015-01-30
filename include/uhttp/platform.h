d7b2f216558469c531c255436f14eda4b9a588ec
Satoshi Konno <skonno@cybergarage.org>
2015-01-30 05:44:21 +0000

    * Fixed to compile normally using gcc 4.8.2 on Ubuntu 14.04.

--- include/uhttp/platform.h
+++ include/uhttp/platform.h
@@ -20,11 +20,14 @@
 #if (defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201103L)) || defined(__APPLE__)
 #define HAVE_STD_SHRAEDPTR 1
 #define uhttp_shared_ptr std::shared_ptr
+#define uhttp_weak_ptr std::weak_ptr
 #endif
 
 #if !defined(HAVE_STD_SHRAEDPTR)
 #include <boost/shared_ptr.hpp>
 #define uhttp_shared_ptr boost::shared_ptr
+#define uhttp_weak_ptr boost::weak_ptr
+#define nullptr NULL
 #endif
 
 #endif
